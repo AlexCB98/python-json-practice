1b9f5bfc313678b9895bbef4d2f0e13420ef584d
Alexandru <lexyus22@gmail.com>
2026-06-15 17:36:45 +0200

    Complete Ex_3

---
+++ Ex_3/main.py
@@ -0,0 +1,10 @@
+import json
+
+
+with open('player.json', 'r') as player:
+    data = json.load(player)
+
+data['health'] = 100
+
+with open('player.json', 'w') as player:
+    json.dump(data, player, indent= 4)
