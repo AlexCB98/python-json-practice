23ad00ecc82d8e79acdd54d1ff76c71ce6374b68
Alexandru <lexyus22@gmail.com>
2026-06-15 18:38:02 +0200

    Complete JSON basics and exception handling

--- Ex_6/main.py
+++ Ex_6/main.py
@@ -1,10 +1,17 @@
 import json
 
-
 try:
     with open('players.json', 'r') as players:
         data = json.load(players)
+
 except FileNotFoundError:
     print('File not found')
+
+except json.JSONDecodeError:
+    print('File empty')
+
 else:
     print('Player loaded successfully')
+
+finally:
+    print('Program finished')
